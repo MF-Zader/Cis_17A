c905a19839b6ebbc5e0ba77fe5438661764d8934
MF-Zader <elijahpinkney7@gmail.com>
2024-08-28 23:31:39 -0700

    Elijah Pinkney

---
+++ IntroLab/HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+/* 
+ * File:   main.cpp
+ * Author: Dr. Mark E. Lehr
+ * Created on August 28, 2024, 7:24 PM
+ * Purpose:  Our First Program
+ *           Hello World Program
+ */
+#include <iostream>  //I/O Library
+using namespace std;
+
+
+int main(int argc, char** argv) {
+    cout<<"Hello World"<<endl;
+    return 0;
+}
